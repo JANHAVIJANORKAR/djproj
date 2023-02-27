bf6888448f1608ce003e4f5ff5d0714c9883cdd1
janhavi janorkar <janhavijanorkar2001@gmail.com>
2023-02-27 19:53:11 +0530

    first commit

---
+++ emp_app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,HttpResponse
+from .models import Employee,Role,Department
+from datetime import datetime
+from django.db.models import Q
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def all_emp(request):
+    emps=Employee.objects.all()
+    context={
+        'emps':emps
+    }
+    print(context)
+    return render(request,'view_all_emp.html',context)
+
+def add_emp(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        salary=int(request.POST['salary'])
+        bonus=int(request.POST['bonus'])
+        phone=int(request.POST['phone'])
+        dept=int(request.POST['dept'])
+        role=int(request.POST['role'])
+        new_emp= Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=bonus,phone=phone,dept_id=dept,role_id=role,hire_date=datetime.now())
+        new_emp.save()
+        return HttpResponse('Employee added sucessfully')
+        
+    elif request.method=='GET':
+        return render(request,'add_emp.html')
+
+    else:
+        return HttpResponse("An Exception occured! Employee has not been added")     
+
+def remove_emp(request,emp_id=0):
+    if emp_id:
+        try:
+            emp_to_be_removed=Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employee removed sucessfully")
+        except:
+            return HttpResponse('PLEASE ENTER VALID EMPLOYEE ID')
+
+
+    emps=Employee.objects.all()
+    context={
+        'emps':emps
+    }
+    return render(request,'remove_emp.html',context)
+   
